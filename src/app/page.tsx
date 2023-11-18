3e58401db4f1d56d096ab9eced3a4bfc330df58d
Gui Silva <adrianon10@gmail.com>
2023-11-18 04:16:29 -0300

    feat: add products type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { IProductProps } from './../types/product-props'
+
+export default async function Home() {
+  const response = await fetch(
+    'https://api.escuelajs.co/api/v1/products?offset=0&limit=10',
+  )
+  const products = await response.json()
+
+  console.log('PRODUCTS: ', products)
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="mx-auto max-w-7xl px-8 pt-8 xl:px-8">
+      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+        {products.map((product: IProductProps) => {
+          return (
+            <div key={product.id}>
+              <h1>{product.title}</h1>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
+
+// https://fakestoreapi.com/products
+// https://api.escuelajs.co/api/v1/products
