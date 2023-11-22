88196bec9fda54da53ec92e6ef0d2cd3bd3af0a5
Gui Silva <adrianon10@gmail.com>
2023-11-22 04:23:12 -0300

    feat: add icon cart

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,12 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ShoppingBasket } from 'lucide-react'
 import Link from 'next/link'
 
+// import { useCartStore } from '@/hooks/store'
+
 export const Header = () => {
+  // const store = useCartStore()
+
   return (
     <nav className="flex w-full items-center justify-between bg-slate-800 px-8 py-2 text-gray-300">
       <Link
@@ -11,16 +16,26 @@ export const Header = () => {
         Virtual Store
       </Link>
       <div className="flex items-center gap-8">
-        <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button className="text-md flex items-center rounded-sm border border-gray-400 px-3 py-2">
-              Fazer Login
-            </button>
-          </SignInButton>
-        </SignedOut>
+        <div className="relative flex items-center">
+          <button type="button">
+            <ShoppingBasket className="h-5 w-5" />
+          </button>
+          <span className="absolute bottom-3 left-3 flex h-5 w-5 items-center justify-center rounded-full bg-teal-600 text-sm font-bold">
+            2
+          </span>
+        </div>
+        <div>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="text-md flex items-center rounded-sm border border-gray-400 px-3 py-2">
+                Fazer Login
+              </button>
+            </SignInButton>
+          </SignedOut>
+        </div>
       </div>
     </nav>
   )
