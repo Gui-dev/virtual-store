b8d49ea918ee901af56188f1089e4fca9e8f6997
Gui Silva <adrianon10@gmail.com>
2023-11-27 05:12:56 -0300

    feat: add infinite scroll component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
-import { IProductProps } from './../types/product-props'
-import { Product } from './components/product'
-import { stripe } from '@/lib/stripe'
+import { fetchProducts } from './actions'
+import { InfiniteScroll } from './components/infinite-scroll'
 
 export default async function Home() {
-  const response = await stripe.products.list()
-  const products: IProductProps[] = await Promise.all(
-    response.data.map(async (product) => {
-      const price = await stripe.prices.list({
-        product: product.id,
-      })
-      return {
-        id: product.id,
-        name: product.name,
-        price: price.data[0].unit_amount,
-        image: product.images[0],
-        description: product.description,
-        currency: price.data[0].currency,
-      }
-    }),
-  )
-
+  const { products } = await fetchProducts({})
   return (
     <div className="mx-auto max-w-7xl px-8 py-8 xl:px-8">
       <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
-        {products.map((product) => {
-          return <Product key={String(product.id)} product={product} />
-        })}
+        <InfiniteScroll initial_products={products} />
       </div>
     </div>
   )
